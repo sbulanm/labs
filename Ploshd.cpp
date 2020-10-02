936730b65ec7d01beac566fadd9411550a292f5e
sbulanm <71441572+sbulanm@users.noreply.github.com>
2020-10-03 02:58:56 +0300

    Add files via upload

---
+++ Ploshd.cpp
@@ -0,0 +1,20 @@
+// Ploshd.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h.>
+using namespace std;
+
+
+int main()
+{
+	float d;
+	cout << "Введите диаметр окружности" << endl;
+	const float p = 3.14;
+	cin >> d;
+	cout << "Длина окружности" << " " << p * d;
+
+
+
+}
+
