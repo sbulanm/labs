90440d605ad031098b433cfe6c5d106136f6ad79
sbulanm <71441572+sbulanm@users.noreply.github.com>
2020-10-03 02:57:23 +0300

    Add files via upload

---
+++ СреднееАрифметическое.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	float a, b;
+	cout << "Введите два числа" << endl;
+	cin >> a >> b;
+	cout << "Среднее арифметическое этих двух чисел=" << (a + b) / 2;
+}
+
